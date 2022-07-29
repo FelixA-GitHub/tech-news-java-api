da9fdc5e8e9b65e523118074e477bc443cd511b2
Your Name <felix.acevedo.tech@gmail.com>
2022-07-28 21:33:54 -0400

    adding User and Post models

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,12 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int var1 = 1;
+        double var2 = 2.2;
+        double sum = var1 + var2;
+        String name = "Felix";
+        String NameInt = name + var1;
+        System.out.println(NameInt);
+    }
+}
